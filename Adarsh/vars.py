b7c97258e23536374a6b3e7a760e920bbbc6989e
KarunaTG <109456795+KarunaTG@users.noreply.github.com>
2022-12-05 10:27:51 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -37,5 +37,5 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://AutoFilter:Karuna15122000@cluster0.qumeb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'wjwueh6'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'TVSeriesCW'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split())) 
