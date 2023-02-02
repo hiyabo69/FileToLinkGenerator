b5658d2ecf6fe50bba8504b705af63941855d2c2
Hiyabo <99443606+hiyabo69@users.noreply.github.com>
2023-02-02 12:05:07 -0500

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,18 +9,18 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '2229357'))
-    API_HASH = str(getenv('API_HASH', '31f183a5a075fd4996cb8ad59e7b794f'))
+    API_ID = int(getenv('API_ID', '13233271'))
+    API_HASH = str(getenv('API_HASH', 'e3ce8145aa657c2a4cc5cf0f7183e476'))
     SHORT_URL = str(getenv('SHORT_URL', 'i5YV1n95'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', '5901953440:AAFCTtag64hVhJuAQAeDf07vlLxw1-_g07c'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '5771404942:AAFS-GEMAJDbtajUbxnyDpDN9rMHEj10_Y4'))
     name = str(getenv('name', 'FileToLinkGenerator'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001220378004'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001651208865'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "400"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "794968418").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "945671168").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'kkaru'))
@@ -36,6 +36,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://AutoFilter:Karuna15122000@cluster0.qumeb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://hiyabo12:informatur26@cluster0.dg2qnqa.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'heiwhs67'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split())) 
