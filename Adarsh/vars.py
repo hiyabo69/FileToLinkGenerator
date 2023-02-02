e25517a2316a746187ed42ea058ebb5264fa79fd
Hiyabo <99443606+hiyabo69@users.noreply.github.com>
2023-02-02 12:20:16 -0500

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -16,7 +16,7 @@ class Var(object):
     name = str(getenv('name', 'FileToLinkGenerator'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001651208865'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001751937436'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "400"))  # 20 minutes
@@ -37,5 +37,5 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://hiyabo12:informatur26@cluster0.dg2qnqa.mongodb.net/?retryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'cinestrenoS3'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'filetome1'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split())) 
