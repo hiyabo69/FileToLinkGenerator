8c6076cd7c4d72b5c6fd003fc4ea1a0639dc38a6
Hiyabo <99443606+hiyabo69@users.noreply.github.com>
2023-02-02 12:14:30 -0500

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -37,5 +37,5 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://hiyabo12:informatur26@cluster0.dg2qnqa.mongodb.net/?retryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'heiwhs67'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'cinestrenoS3'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split())) 
